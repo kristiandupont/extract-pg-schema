f34f31cd7cb9cf6db14f66e3a4a6f45fed61c9e5
Kristian Dupont <kristian@kristiandupont.com>
2020-10-30 22:37:07 +0100

    Split into multiple files

---
+++ src/extract-views.js
@@ -0,0 +1,38 @@
+import Knex from 'knex'; // import type
+import parseComment from './parseComment';
+import extractColumns from './extract-columns';
+
+/**
+ * @param {string} schemaName
+ * @param {Knex} db
+ * @returns {Promise<import('./types').TableOrView[]>}
+ */
+async function extractViews(schemaName, db) {
+  /** @type {import('./types').TableOrView[]} */
+  const views = [];
+  const dbViews = await db
+    .select('viewname')
+    .from('pg_catalog.pg_views')
+    .where('schemaname', schemaName);
+
+  for (const view of dbViews) {
+    const viewName = view.viewname;
+    const viewCommentQuery = await db.schema.raw(
+      `SELECT obj_description('"${schemaName}"."${viewName}"'::regclass)`
+    );
+    const rawViewComment =
+      viewCommentQuery.rows.length > 0 &&
+      viewCommentQuery.rows[0].obj_description;
+
+    const columns = await extractColumns(schemaName, viewName, db);
+
+    views.push({
+      name: viewName,
+      ...parseComment(rawViewComment),
+      columns,
+    });
+  }
+  return views;
+}
+
+export default extractViews;
