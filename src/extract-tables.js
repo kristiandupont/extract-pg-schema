022e2b2453a0fcc3d0589a13aa56f85643024e49
Krisan Alfa Timur <krisan47@gmail.com>
2022-06-03 15:48:30 +0800

    feat: added ability to extract selected tables (#197)

--- src/extract-tables.js
+++ src/extract-tables.js
@@ -4,11 +4,12 @@ import parseComment from './parse-comment';
 /**
  * @param {string} schemaName
  * @param {import('knex').Knex<any, unknown[]>} db
+ * @param {string[]} tables
  * @returns {Promise<import('./types').TableOrView[]>}
  */
-async function extractTables(schemaName, db) {
+async function extractTables(schemaName, db, tables = []) {
   /** @type {import('./types').TableOrView[]} */
-  const tables = [];
+  const extractedTables = [];
   // Exclude partition tables
   const dbTables = await db
     .select('tablename')
@@ -16,7 +17,12 @@ async function extractTables(schemaName, db) {
     .from('pg_catalog.pg_tables')
     .join('pg_catalog.pg_class', 'tablename', '=', 'pg_class.relname')
     .where('schemaname', schemaName)
-    .andWhere('relispartition', '=', false);
+    .andWhere('relispartition', '=', false)
+    .modify((q) => {
+      if (!tables.length) return;
+
+      q.whereIn('tablename', tables);
+    });
 
   for (const table of dbTables) {
     const tableName = table.tablename;
@@ -29,14 +35,14 @@ async function extractTables(schemaName, db) {
 
     const columns = await extractColumns(schemaName, tableName, db);
 
-    tables.push({
+    extractedTables.push({
       name: tableName,
       ...parseComment(rawTableComment),
       columns,
     });
   }
 
-  return tables;
+  return extractedTables;
 }
 
 export default extractTables;
