a8281ffd29dd1475208c5f3c3d0cc27e0ddd2bd7
Kristian Dupont <kristian@kristiandupont.com>
2022-07-02 12:05:47 +0000

    Introduce docs

---
+++ docs/.vitepress/config.js
@@ -0,0 +1,18 @@
+export default {
+  title: 'extract-pg-schema',
+  description: 'Just playing around.',
+  themeConfig: {
+    sidebar: [
+      {
+        text: 'Guide',
+        items: [{ text: 'API', link: '/api/' }],
+      },
+    ],
+    socialLinks: [
+      {
+        icon: 'github',
+        link: 'https://github.com/kristiandupont/extract-pg-schema',
+      },
+    ],
+  },
+};
