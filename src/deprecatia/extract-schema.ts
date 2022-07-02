59c2b6ece986f6eac4a37c8691c57ed298cda6ec
Kristian Dupont <kristian@kristiandupont.com>
2022-07-02 11:13:28 +0000

    Add a bit of backwards compatibility

--- src/deprecatia/extract-schema.ts
+++ src/deprecatia/extract-schema.ts
@@ -2,6 +2,24 @@ import { ConnectionConfig } from 'pg';
 
 import extractSchemas from '../extractSchemas';
 
+const mapColumn = (column: any) => ({
+  ...column,
+  nullable: column.isNullable,
+  rawInfo: column.informationSchemaValue,
+  isPrimary: column.isPrimaryKey,
+  references: column.references?.map((r: any) => ({
+    ...r,
+    schema: r.schemaName,
+    table: r.tableName,
+    column: r.columnName,
+  })),
+});
+
+const mapTable = (table: any) => ({
+  ...table,
+  columns: table.columns.map(mapColumn),
+});
+
 /** @deprecated - use extractSchemas instead */
 const extractSchema = async (
   schemaName: string,
@@ -14,10 +32,20 @@ const extractSchema = async (
   const r = await extractSchemas(connectionConfig, {
     schemas: [schemaName],
     resolveViews,
+    typeFilter: (pgType) => {
+      if (!['table', 'view', 'enum', 'compositeType'].includes(pgType.kind))
+        return false;
+
+      if (tables && pgType.kind === 'table') {
+        return tables.includes(pgType.name);
+      }
+      return true;
+    },
   });
+
   const result = {
-    tables: r[schemaName].table,
-    views: r[schemaName].view,
+    tables: r[schemaName].table.map(mapTable),
+    views: r[schemaName].view.map(mapTable),
     types: [
       ...(r[schemaName].enum ?? []),
       ...(r[schemaName].compositeType ?? []),
