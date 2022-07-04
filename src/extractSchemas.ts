9f6c17a24a44f52556fa192f0e0bd66dcaf9e654
Kristian Dupont <kristian@kristiandupont.com>
2022-07-04 04:03:16 +0000

    Documenting comments

--- src/extractSchemas.ts
+++ src/extractSchemas.ts
@@ -15,41 +15,46 @@ import extractTable, { TableDetails } from './kinds/extractTable';
 import extractView, { ViewDetails } from './kinds/extractView';
 import fetchTypes from './kinds/fetchTypes';
 import PgType, { Kind } from './kinds/PgType';
-// import resolveViewColumns from './resolveViewColumns';
 
-export type Details =
-  | DomainDetails
-  | EnumDetails
-  | RangeDetails
-  | TableDetails
-  | ViewDetails
-  | MaterializedViewDetails
-  | CompositeTypeDetails;
+interface DetailsMap {
+  domain: DomainDetails;
+  enum: EnumDetails;
+  range: RangeDetails;
+  table: TableDetails;
+  materializedView: MaterializedViewDetails;
+  view: ViewDetails;
+  compositeType: CompositeTypeDetails;
+}
 
-export type PopulatedType = PgType & Details;
-
+/**
+ * extractSchemas generates a record of all the schemas extracted, indexed by schema name.
+ * The schemas are instances of this type.
+ */
 export type Schema = {
   name: string;
-  domain: PopulatedType[];
-  enum: PopulatedType[];
-  range: PopulatedType[];
-  table: PopulatedType[];
-  view: PopulatedType[];
-  materializedView: PopulatedType[];
-  compositeType: PopulatedType[];
+  domains: DomainDetails[];
+  enums: EnumDetails[];
+  ranges: RangeDetails[];
+  tables: TableDetails[];
+  views: ViewDetails[];
+  materializedViews: MaterializedViewDetails[];
+  compositeTypes: CompositeTypeDetails[];
 };
 
 const emptySchema: Omit<Schema, 'name'> = {
-  domain: [],
-  enum: [],
-  range: [],
-  table: [],
-  view: [],
-  materializedView: [],
-  compositeType: [],
+  domains: [],
+  enums: [],
+  ranges: [],
+  tables: [],
+  views: [],
+  materializedViews: [],
+  compositeTypes: [],
 };
 
-type Populator = (db: Knex, pgType: PgType) => Promise<Details>;
+type Populator = <K extends Kind>(
+  db: Knex,
+  pgType: PgType<K>
+) => Promise<DetailsMap[K]>;
 
 // @ts-ignore Why is this broken? I don't understand. :-/
 const populatorMap: Record<Kind, Populator> = {
@@ -63,15 +68,55 @@ const populatorMap: Record<Kind, Populator> = {
   compositeType: extractCompositeType,
 } as const;
 
-export type ExtractSchemaOptions = {
+/**
+ * This is the options object that can be passed to `extractSchemas`.
+ * @see extractSchemas
+ */
+export interface ExtractSchemaOptions {
+  /**
+   * Will contain an array of schema names to extract.
+   * If undefined, all non-system schemas will be extracted.
+   */
   schemas?: string[];
-  typeFilter?: (pgType: PgType) => boolean;
-  resolveViews?: boolean;
-  onProgressStart?: (total: number) => void;
-  onProgress?: () => void;
-  onProgressEnd?: () => void;
-};
 
+  /**
+   * Filter function that you can use if you want to exclude
+   * certain items from the schemas.
+   */
+  typeFilter?: (pgType: PgType) => boolean;
+
+  /**
+   * extractShemas will always attempt to parse view definitions to
+   * discover the "source" of each column, i.e. the table or view that it
+   * is derived from.
+   * If this option is set to `true`, it will attempt to follow this
+   * source and copy values like indices, isNullable, etc.
+   * so that the view data is closer to what the database reflects.
+   */
+  resolveViews?: boolean;
+
+  /**
+   * Called with the number of types to extract.
+   */
+  onProgressStart?: (total: number) => void;
+
+  /**
+   * Called once for each type that is extracted.
+   */
+  onProgress?: () => void;
+
+  /**
+   * Called when all types have been extracted.
+   */
+  onProgressEnd?: () => void;
+}
+
+/**
+ * Perform the extraction
+ * @param connectionConfig - Connection string or configuration object for Postgres connection
+ * @param options - Optional options
+ * @returns A record of all the schemas extracted, indexed by schema name.
+ */
 async function extractSchemas(
   connectionConfig: string | ConnectionConfig,
   options?: ExtractSchemaOptions
@@ -108,22 +153,22 @@ async function extractSchemas(
   }
 
   const populated = await Promise.all(
-    typesToExtract.map(async (pgType) => {
-      const populator = populatorMap[pgType.kind];
-      const details = await populator(db, pgType);
-      return { ...pgType, ...details };
-    })
+    typesToExtract.map(async (pgType) => populatorMap[pgType.kind](db, pgType))
   );
 
   const schemas: Record<string, Schema> = {};
-  populated.forEach((p: PgType & Details) => {
+  populated.forEach((p) => {
     if (!(p.schemaName in schemas)) {
       schemas[p.schemaName] = {
         name: p.schemaName,
         ...emptySchema,
       };
     }
-    schemas[p.schemaName][p.kind] = [...schemas[p.schemaName][p.kind], p];
+    // @ts-ignore
+    schemas[p.schemaName][`${p.kind}s`] = [
+      ...schemas[p.schemaName][`${p.kind}s`],
+      p,
+    ];
   });
 
   await db.destroy();
