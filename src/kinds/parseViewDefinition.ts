22ecf6ee122ca9257395d2a1f077bed04bf379f2
Kristian Dupont <kristian@kristiandupont.com>
2023-12-03 19:49:09 +0100

    Support view definitions with WITH clauses (#453)
    * Add a test for WITH clauses
    
    * Extract select statement processing
    
    * Simple with clauses working, Kanel#481 still failing
    
    * Make test succeed
    
    * Ignore linter issues
    
    * Remove log stmt

--- src/kinds/parseViewDefinition.ts
+++ src/kinds/parseViewDefinition.ts
@@ -13,6 +13,60 @@ export type ViewReference = {
     | undefined;
 };
 
+function parseSelectStmt(
+  selectAst: any, // Change the type to match the AST structure
+  defaultSchema: string,
+  aliases: { [alias: string]: { schema: string; table: string } },
+): ViewReference[] {
+  if (selectAst.larg && selectAst.rarg) {
+    // This is a UNION, INTERSECT, or EXCEPT operation
+    return [
+      ...parseSelectStmt(selectAst.larg.SelectStmt, defaultSchema, aliases),
+      ...parseSelectStmt(selectAst.rarg.SelectStmt, defaultSchema, aliases),
+    ];
+  }
+
+  const viewReferences: ViewReference[] = [];
+
+  // eslint-disable-next-line unicorn/no-array-for-each
+  selectAst.fromClause.forEach((fromClause: any) => {
+    const fromTable = fromClause.RangeVar;
+
+    const selectTargets = jp.query(selectAst, "$.targetList[*].ResTarget");
+
+    // eslint-disable-next-line unicorn/no-array-for-each
+    selectTargets.forEach((selectTarget: any) => {
+      const fields = jp.query(selectTarget, "$.val[*].fields[*].String.str");
+      let sourceTable = fromTable?.relname;
+      let sourceSchema = fromTable?.schemaname;
+      if (fields.length === 2) {
+        const tableRel = fields[0];
+        if (tableRel in aliases) {
+          sourceTable = aliases[tableRel].table;
+          sourceSchema = aliases[tableRel].schema ?? sourceSchema;
+        } else {
+          sourceTable = tableRel;
+        }
+      }
+      const sourceColumn = last(fields);
+
+      const viewReference: ViewReference = {
+        viewColumn: selectTarget.name || last(fields),
+        source:
+          sourceTable && sourceColumn
+            ? {
+                schema: sourceSchema ?? defaultSchema,
+                table: sourceTable,
+                column: last(fields),
+              }
+            : undefined,
+      };
+      viewReferences.push(viewReference);
+    });
+  });
+
+  return viewReferences;
+}
 function parseViewDefinition(
   selectStatement: string,
   defaultSchema: string,
@@ -26,8 +80,6 @@ function parseViewDefinition(
     );
   }
 
-  const firstFromTable = selectAst.fromClause[0].RangeVar;
-
   const aliasDefinitions = jp.query(
     ast,
     "$.RawStmt.stmt.SelectStmt.fromClause..[?(@.alias)]",
@@ -40,37 +92,40 @@ function parseViewDefinition(
     ]),
   );
 
-  const selectTargets = jp.query(
-    ast,
-    "$.RawStmt.stmt.SelectStmt.targetList[*].ResTarget",
-  );
-  const viewReferences = selectTargets.map((selectTarget) => {
-    const fields = jp.query(selectTarget, "$.val[*].fields[*].String.str");
-    let sourceTable = firstFromTable?.relname;
-    let sourceSchema = firstFromTable?.schemaname;
-    if (fields.length === 2) {
-      const tableRel = fields[0];
-      if (tableRel in aliases) {
-        sourceTable = aliases[tableRel].table;
-        sourceSchema = aliases[tableRel].schema ?? sourceSchema;
-      } else {
-        sourceTable = tableRel;
-      }
-    }
-    const sourceColumn = last(fields);
+  const withClauses = selectAst.withClause?.WithClause?.ctes || [];
 
-    const viewReference: ViewReference = {
-      viewColumn: selectTarget.name || last(fields),
-      source:
-        sourceTable && sourceColumn
-          ? {
-              schema: sourceSchema ?? defaultSchema,
-              table: sourceTable,
-              column: last(fields),
-            }
-          : undefined,
-    };
-    return viewReference;
+  const cteAliases: Record<string, ViewReference[]> = {};
+
+  for (const cte of withClauses) {
+    if (cte.CommonTableExpr) {
+      const alias = cte.CommonTableExpr.ctename;
+      const cteQuery = cte.CommonTableExpr.ctequery.SelectStmt;
+
+      if (!cteQuery) {
+        continue;
+      }
+
+      // Process the CTE query recursively
+      const cteAlias = parseSelectStmt(cteQuery, defaultSchema, aliases);
+
+      cteAliases[alias] = cteAlias;
+    }
+  }
+
+  const primaryViewReferences = parseSelectStmt(
+    selectAst,
+    defaultSchema,
+    aliases,
+  );
+
+  const viewReferences = primaryViewReferences.map((viewReference) => {
+    const source = viewReference.source;
+    return source && source.table in cteAliases
+      ? (cteAliases[source.table].find(
+          (cteViewReference) =>
+            cteViewReference.viewColumn === viewReference.viewColumn,
+        ) as ViewReference)
+      : viewReference;
   });
 
   return viewReferences;
