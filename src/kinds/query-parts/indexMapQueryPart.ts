d1c8cd22533699e73d7b64d48e685960f3651766
orangain <orangain@gmail.com>
2024-01-27 20:56:14 +0900

    Fix #481, Don't extract indices from another schema

--- src/kinds/query-parts/indexMapQueryPart.ts
+++ src/kinds/query-parts/indexMapQueryPart.ts
@@ -10,14 +10,17 @@ const indexMapQueryPart = `
     pg_class t,
     pg_class i,
     pg_index ix,
-    pg_attribute a
+    pg_attribute a,
+    pg_namespace n
   WHERE
     t.oid = ix.indrelid
     AND i.oid = ix.indexrelid
     AND a.attrelid = t.oid
+    AND n.oid = t.relnamespace
     AND a.attnum = ANY (ix.indkey)
     AND t.relkind = 'r'
     AND t.relname = :table_name
+    AND n.nspname = :schema_name
   GROUP BY
     a.attname
 `;
