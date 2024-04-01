5f0f9eb1b3bdfdd14a5c7181ab3e5692a5193eb9
Kristian Dupont <kristian@kristiandupont.com>
2024-04-01 09:48:30 +0000

    Fix vitest cfg

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,9 +5,6 @@ export default defineConfig({
   test: {
     include: ["src/**/*.{test,spec}.{js,mjs,cjs,ts,mts,cts,jsx,tsx}"],
     globalSetup: "src/tests/globalSetup.ts",
-    deps: {
-      inline: ["vitest-fixture"],
-    },
     sequence: { hooks: "stack" },
   },
 });
